9a90ea3295433f543cec9a4be22a1e6dfd6b770e
Kevin <esfk070@gmail.com>
2024-08-09 16:52:29 -0600

    Configure and test Sprint

--- src/test/java/com/skilldistillery/film/controllers/FilmController.java
+++ src/test/java/com/skilldistillery/film/controllers/FilmController.java
@@ -1,13 +1,15 @@
 package com.skilldistillery.film.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class FilmController {
-	
+
+	@RequestMapping(path = { "index.do", "/" })
 	public String index() {
-		
-		return "index";
+
+		return "WEB-INF/index.jsp";
 	}
 
 }
