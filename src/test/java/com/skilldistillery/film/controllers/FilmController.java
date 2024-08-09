cf630548be7fc06eabb0d9f62885b3eec7f85250
“Justin” <jdenton.bills@gmail.com>
2024-08-09 17:52:01 -0500

    film controller

--- src/test/java/com/skilldistillery/film/controllers/FilmController.java
+++ src/test/java/com/skilldistillery/film/controllers/FilmController.java
@@ -1,10 +1,12 @@
 package com.skilldistillery.film.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class FilmController {
 	
+	@RequestMapping( path={"index.do", "/"} )
 	public String index() {
 		
 		return "index";
